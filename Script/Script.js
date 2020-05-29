016a971dfcbcd6a639e82825bd406cfa3af20992
Patrick Miglar <miglar.patrick@gmail.com>
2020-05-29 18:49:12 +0200

    script rework

--- Script/Script.js
+++ Script/Script.js
@@ -2,25 +2,25 @@ function calculateInsurance() {
     var name = document.getElementById("name").value;
     var age = Number(document.getElementById("age").value);
     var country = document.getElementById("country").value;
-    var hp = document.getElementById("hp").value;
+    var hpower = document.getElementById("hpower").value;
 
     switch (true) {
         case (country == "Austria"):
-            i = Math.round((hp * 100) / (age + 50));
+            i = Math.round((hpower * 100) / (age + 50));
 
             document.getElementById('output').innerHTML = name + ", your insurance costs " + i + " €";
 
             break;
         case (country == "Hungary"):
-            i = Math.round((hp * 120) / (age + 100));
+            i = Math.round((hpower * 120) / (age + 100));
             document.getElementById('output').innerHTML = name + ", your insurance costs " + i + " €";
             break;
         case (country == "Greece"):
-            i = Math.round((hp * 150) / ((age + 3) + 50));
+            i = Math.round((hpower * 150) / ((age + 3) + 50));
             document.getElementById('output').innerHTML = name + ", your insurance costs " + i + " €";
             break;
         default:
-            error = "Error  -  review your javascript";
+            error = "Error";
             console.log(error);
             break;
     }
